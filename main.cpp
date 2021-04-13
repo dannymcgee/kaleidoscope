7c5cf7379f25a5905d6075299de7720c6b158877
Danny McGee <dannymcgee@gmail.com>
2021-04-12 22:37:03 -0400

    Starting over with Lox (and an intelligible parser architecture)

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
-#include "lexer/lexer.h"
-#include "parser/parser.h"
-#include "repl/repl.hpp"
+#include <iostream>
 
 
 int main() {
-	while (true) {
-		REPL::Prompt();
-		Parser::GetNextToken();
-
-		switch (Parser::s_CurTok) {
-			case Lexer::Token::Eof:
-				return 0;
-			case Lexer::Token::Def:
-				REPL::HandleDefinition();
-				break;
-			case Lexer::Token::Extern:
-				REPL::HandleExtern();
-				break;
-			default:
-				if (static_cast<char>(Parser::s_CurTok) == ';') {
-					Parser::GetNextToken();
-					break;
-				}
-				REPL::HandleTopLevelExpr();
-				break;
-		}
-	}
+	std::cout << "Hello, Lox!" << std::endl;
 }
